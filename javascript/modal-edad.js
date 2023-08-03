13e88ca252a790aa437e7cbb893449ea7f8a4011
Gianluca Rottino <gianlucarottino@gmail.com>
2023-08-03 02:27:01 -0300

    Cree pestaña Crear cuenta

--- javascript/modal-edad.js
+++ javascript/modal-edad.js
@@ -1,42 +1,87 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Mostrar la ventana modal después de 2 segundos (2000 milisegundos)
-  let modal = document.getElementById("modal");
-  setTimeout(() =>{
-    modal.style.visibility = "visible";
-  }, 1000);
+document.addEventListener("DOMContentLoaded", () => {
+    let modal = document.getElementById("modal");
+    setTimeout(() => {
+        modal.style.visibility = "visible";
+    }, 1000);
 
-  let yesBtn = document.getElementById("yesBtn");
-  yesBtn.addEventListener("click", () => {
+    let yesBtn = document.getElementById("yesBtn");
+    yesBtn.addEventListener("click", () => {
+        // modal.style.visibility = "hidden";
+        onBoarding();
+    });
 
-    modal.style.visibility = "hidden";
-
-  })
-
-  let noBtn = document.getElementById("noBtn");
-  noBtn.addEventListener("click", () => {
-
-    let sin_permiso = sinPermiso();
-
-  })
+    let noBtn = document.getElementById("noBtn");
+    noBtn.addEventListener("click", () => {
+        sinPermiso();
+    });
 });
 
+function nuevoModal(
+    url_img,
+    text_titulo,
+    text_parrafo,
+    button_text,
+    button_id
+) {
+    let modal_contenido = document.getElementById("modalCtn");
+    modal_contenido.innerHTML = "";
+
+    let div_modal_texto = document.createElement("div");
+    div_modal_texto.classList = "modal-texto";
+    modal_contenido.appendChild(div_modal_texto);
+
+    let ilustracion = document.createElement("img");
+    ilustracion.src = url_img;
+    modal_contenido.appendChild(ilustracion);
+
+    let titulo = document.createElement("h2");
+    titulo.classList = "modal-title";
+    titulo.textContent = text_titulo;
+    modal_contenido.appendChild(titulo);
+
+    let parrafo = document.createElement("p");
+    parrafo.textContent = text_parrafo;
+    modal_contenido.appendChild(parrafo);
+
+    let div_modal_btn = document.createElement("div");
+    div_modal_btn.classList = "modal-botones";
+    modal_contenido.appendChild(div_modal_btn);
+
+    let button = document.createElement("button");
+    button.classList = "btn prim";
+    button.textContent = button_text;
+    button.id = button_id;
+    div_modal_btn.appendChild(button);
+}
+
+function onBoarding() {
+    let url_img = "./images/done-pana.png";
+    let text_titulo = "¡Que bueno conocerte!";
+    let text_parrafo =
+        "Maximiza tu experiencia: Create una cuenta ahora y descubre todas las funciones de nuestra aplicación. ¡Bienvenido(a)!";
+    let button_text = "¡Crear cuenta ahora!";
+    let button_id = "loginBtn";
+
+    nuevoModal(url_img, text_titulo, text_parrafo, button_text, button_id);
+
+		button = document.getElementById("loginBtn");
+    button.addEventListener("click", () => {
+				window.location.href = "./pages/signup.html";
+    });
+}
+
 function sinPermiso() {
+    let url_img = "./images/feeling-sorry-cuate.png";
+    let text_titulo = "¡Que lastima que no lo seas!";
+    let text_parrafo =
+        "No podemos dejarte ingresar, es un sitio web de bebidas alcoholicas";
+    let button_text = "Cerrar pagina";
+    let button_id = "exitBtn";
 
-  let modal_contenido = document.getElementById("modalCtn")
-  modal_contenido.innerHTML = "";
-  
-  let ilustracion_error = document.createElement("img");
-  let urlImg = "./images/feeling-sorry-cuate.png"
-  ilustracion_error.src = urlImg;
-  modal_contenido.appendChild(ilustracion_error);
+    nuevoModal(url_img, text_titulo, text_parrafo, button_text, button_id);
 
-  let titulo_error = document.createElement("h2");
-  titulo_error.classList = "modal-title";
-  titulo_error.textContent = "¡Que lastima que no lo seas!";
-  modal_contenido.appendChild(titulo_error);
-
-  let parrafo_error = document.createElement("p");
-  parrafo_error.textContent = "No podemos dejarte ingresar, es un sitio web de bebidas alcoholicas";
-  modal_contenido.appendChild(parrafo_error);
-
+    button = document.getElementById("exitBtn");
+    button.addEventListener("click", () => {
+        window.close();
+    });
 }
